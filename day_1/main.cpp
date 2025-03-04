0df6db4d54684f97b63053b0584d103d889204ad
Maryam Ruqsha <maryamruqsha16@gmail.com>
2025-03-04 13:45:32 +0530

    My first commit

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -1,1 +1,11 @@
 // day 1
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Ruqsha!" << std::endl;
+    std::cout << "Hello, Ruqsha!" << std::endl;
+    std::cout << "Hello, Ruqsha!" << std::endl;
+    std::cout << "Hello, Ruqsha!" << std::endl;
+    return 0;
+    
+}
---
+++ print.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <iostream>
+
+void print(const std::string& message);
+
+#endif // PRINT_H
